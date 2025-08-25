996a0b5f91ec84315aac9536add8093989ac87b0
hexdhog <hexdhog@gmail.com>
2025-08-25 16:17:36 +0200

    fixed MNIST training speed & convengence

--- mnist/mnist.py
+++ mnist/mnist.py
@@ -6,8 +6,10 @@ import gzip
 import struct
 from pathlib import Path
 from urllib import request
-from tinygrad import Tensor, TinyJit, nn, Device
+from tinygrad import Tensor, TinyJit, nn, dtypes
 from matplotlib import pyplot as plt
+
+Tensor.manual_seed(69420)
 
 N_LABELS = 10
 IMG_RES = 28
@@ -32,7 +34,7 @@ def load_images(path) -> Tensor:
     assert struct.unpack("I", gz.read(4))[0] == 0x3080000, "invalid file format" # check magic number
     n, nrow, ncol = struct.unpack(">III", gz.read(12))
     assert nrow == ncol == IMG_RES, f"unexpected number of rows/columns ({nrow}/{ncol} != {IMG_RES})"
-    return Tensor(gz.read(n * nrow * ncol)).view((n, nrow, ncol)).realize()
+    return Tensor(gz.read(n * nrow * ncol)).view((n, nrow, ncol)).cast(dtypes.float16).realize()
 
 def load_labels(path) -> Tensor:
   with gzip.open(path, "rb") as gz:
@@ -42,13 +44,14 @@ def load_labels(path) -> Tensor:
 
 # ******************** MLP ********************
 
-class MLP:
+class MLPRaw:
   def __init__(self, n_hidden: int):
-    self.w1 = Tensor.randn(IMG_RES ** 2, n_hidden, requires_grad=True)
-    self.b1 = Tensor.randn(n_hidden, requires_grad=True)
-    self.w2 = Tensor.randn(n_hidden, N_LABELS, requires_grad=True)
-    self.b2 = Tensor.randn(N_LABELS, requires_grad=True)
-    self.params = [self.w1, self.b1, self.w2, self.b2]
+    bound1 = 1 / math.sqrt(IMG_RES**2)
+    self.w1 = Tensor.uniform(IMG_RES ** 2, n_hidden, low=-bound1, high=bound1, requires_grad=True)
+    self.b1 = Tensor.uniform(n_hidden, low=-bound1, high=bound1, requires_grad=True)
+    bound2 = 1 / math.sqrt(n_hidden)
+    self.w2 = Tensor.uniform(n_hidden, N_LABELS, low=-bound2, high=bound2, requires_grad=True)
+    self.b2 = Tensor.uniform(N_LABELS, low=-bound2, high=bound2, requires_grad=True)
 
   def __call__(self, x: Tensor) -> Tensor:
     B, W, H = x.shape
@@ -56,6 +59,20 @@ class MLP:
     x = x.view(B, W * H)
     x = Tensor.tanh(x @ self.w1 + self.b1)
     x = Tensor.tanh(x @ self.w2 + self.b2)
+    return x
+
+class MLP:
+  def __init__(self, n_hidden: int):
+    self.l1 = nn.Linear(IMG_RES ** 2, n_hidden)
+    self.l2 = nn.Linear(n_hidden, N_LABELS)
+    self.params = nn.state.get_parameters(self)
+
+  def __call__(self, x: Tensor) -> Tensor:
+    B, W, H = x.shape
+    assert W == H == IMG_RES
+    x = x.view(B, W * H)
+    x = self.l1(x).tanh()
+    x = self.l2(x).tanh()
     return x
 
 if __name__ == "__main__":
@@ -66,7 +83,7 @@ if __name__ == "__main__":
 
   # using continuous batching as random indexing is very slow
   # in production the data should be randomized first
-  n_batch = 256
+  n_batch = 4096
   batch_idx = 0
 
   def get_batch(x: Tensor, y: Tensor):
@@ -77,26 +94,14 @@ if __name__ == "__main__":
     batch_idx = (start + n_batch) % n
     return x[start:start+n_batch].contiguous(), y[start:start+n_batch].contiguous()
 
-  m = MLP(128)
-  print(f"number of parameters: {sum(p.numel() for p in m.params)}")
+  # m = MLP(64)
+  m = MLPRaw(64)
+  params = nn.state.get_parameters(m)
+  print(f"number of parameters: {sum(p.numel() for p in params)}")
 
-  # using tinygrad's builtin SGD as it's faster than manually updating the parameters
-  optimizer = nn.optim.SGD(m.params, lr=0.25)
-
-  # lr = 0.1
-  # 82.05041775 seconds
-  # train loss: 0.92101359
-  # test loss: 0.98057789
-
-  # lr = 0.25
-  # 175.11699254 seconds
-  # train loss: 0.89867193
-  # test loss: 0.96225542
-
-  # lr = 0.5
-  # 82.05041775 seconds
-  # train loss: 0.92101359
-  # test loss: 0.98057789
+  # using tinygrad's builtin optimizers as it's faster than manually updating the parameters
+  lr_init = 0.005
+  optimizer = nn.optim.AdamW(params, lr=lr_init)
 
   @TinyJit
   @Tensor.train()
@@ -105,15 +110,14 @@ if __name__ == "__main__":
     logits = m(x)
     loss = logits.cross_entropy(y)
     loss.backward()
-    optimizer.step()
-    return loss.realize()
+    return loss.realize(*optimizer.schedule_step())
 
   start = time.perf_counter()
-  n_iter = 100000
-  # n_iter = 20000
+  n_iter = 15000
   for i in range(n_iter):
-    loss = step(*get_batch(X_train, Y_train))
-    if i % 10000 == 0: print(f"{i:10d} loss: {loss.item():.8f}")
+    x, y = get_batch(X_train, Y_train)
+    loss = step(x, y)
+    if i % 1000 == 0: print(f"{i:10d} loss: {loss.item():.8f}")
   end = time.perf_counter()
   print(f"{end - start:.8f} seconds")
 
@@ -123,7 +127,7 @@ if __name__ == "__main__":
   print(f"train loss: {train_loss:.8f}")
   print(f"test loss: {test_loss:.8f}")
 
-  n_gen = 24
+  n_gen = 40
   ix = Tensor.randint(n_gen, low=0, high=len(X_train))
   x, y = X_train[ix], Y_train[ix]
 
